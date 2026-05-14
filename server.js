7349a2daccf2f344f70268d33d3282eeabd9cff4
siddiquimukhim <siddiquimukhim@gmail.com>
2026-05-14 18:54:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const { ExpressPeerServer } = require("peer");
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
 
 const app = express();
 
-const server = app.listen(process.env.PORT || 10000, () => {
-    console.log("PeerJS Server Running");
+const server = app.listen(process.env.PORT || 9000, () => {
+    console.log('PeerJS Server Running');
+});
+
+app.get('/', (req, res) => {
+    res.send('PeerJS Server Running');
 });
 
 const peerServer = ExpressPeerServer(server, {
-    debug: true
+    debug: true,
+    path: '/'
 });
 
-app.use("/peerjs", peerServer);
-
-app.get("/", (req, res) => {
-    res.send("PeerJS Running");
-});
+app.use('/peerjs', peerServer);
